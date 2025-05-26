aee5d44c7f93ed3fc143eb4fd30f4ecd1d2d6718
Janek <janek.uchman@hyperluminalgames.com>
2025-05-26 22:16:45 +0100

    Adding simple main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+namespace Bomberman.UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private InputActionReference _startInput;
+
+        private void OnEnable()
+        {
+            _startInput.action.performed += StartGame;
+        }
+
+        private void OnDisable()
+        {
+            _startInput.action.performed -= StartGame;
+        }
+
+        private void StartGame(InputAction.CallbackContext obj)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
